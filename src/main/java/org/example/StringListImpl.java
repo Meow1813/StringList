26fb6578cc22165a0b4765315e3e8376aad63a5d
Alex <shurik228154@mail.ru>
2023-05-24 21:32:28 +0300

    Сделанное ДЗ

---
+++ src/main/java/org/example/StringListImpl.java
@@ -0,0 +1,153 @@
+package org.example;
+
+import org.example.Exception.ElementNotExistException;
+import org.example.Exception.InvalidIndexException;
+import org.example.Exception.InvalidItemException;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class StringListImpl implements StringList {
+
+    private String[] storage;
+    private int size = 0;
+
+    public StringListImpl() {
+        storage = new String[10];
+    }
+
+    public StringListImpl(int size) {
+        storage = new String[size];
+    }
+
+    @Override
+    public String add(String item) {
+
+        checkItem(item);
+        checkSize();
+        storage[size] = item;
+        size++;
+        return item;
+    }
+
+    @Override
+    public String add(int index, String item) {
+        checkItem(item);
+        checkSize();
+        if(index == size){
+            add(item);
+        }
+        checkIndex(index);
+        size++;
+        for (int i = size - 1; i > index; i--) {
+            storage[i] = storage[i - 1];
+        }
+        storage[index] = item;
+        return item;
+    }
+
+    @Override
+    public String set(int index, String item) {
+        checkIndex(index);
+        checkItem(item);
+        storage[index] = item;
+        return item;
+    }
+
+    @Override
+    public String remove(String item) {
+        checkItem(item);
+        if(contains(item)){
+           return remove(indexOf(item));
+        }
+       throw new ElementNotExistException();
+    }
+
+    @Override
+    public String remove(int index) {
+        checkIndex(index);
+        String result = storage[index];
+        for (int i = index; i < size - 1; i++) {
+            storage[i] = storage[i+1];
+        }
+        size--;
+        return result;
+    }
+
+    @Override
+    public boolean contains(String item) {
+        checkItem(item);
+        return indexOf(item)!=-1;
+    }
+
+    @Override
+    public int indexOf(String item) {
+        checkItem(item);
+        for (int i = 0; i < size; i++) {
+            if (storage[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(String item) {
+        checkItem(item);
+        for (int i = size - 1; i >= 0; i--) {
+            if (storage[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public String get(int index) {
+        checkIndex(index);
+        return storage[index];
+    }
+
+    @Override
+    public boolean equals(StringList otherList) {
+        return Arrays.equals(this.toArray(), otherList.toArray());
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+    }
+
+    @Override
+    public String[] toArray() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    private void checkItem(String item) {
+        if (Objects.isNull(item)) {
+            throw new InvalidItemException();
+        }
+    }
+
+    private void checkSize() {
+        if (size == storage.length) {
+            storage = Arrays.copyOf(storage, (int) (size * 1.5 + 1));
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new InvalidIndexException();
+        }
+    }
+}
---
+++ src/test/java/org/example/StringListImplTest.java
@@ -0,0 +1,155 @@
+package org.example;
+
+
+import org.example.Exception.ElementNotExistException;
+import org.example.Exception.InvalidIndexException;
+import org.example.Exception.InvalidItemException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringListImplTest {
+    StringListImpl list = new StringListImpl(5);
+
+    @BeforeEach
+    public void fillList(){
+        list.add("test");
+        list.add("string");
+        list.add("list");
+        list.add("word");
+        list.add("string");
+    }
+    @AfterEach
+    public void clearList(){
+        list.clear();
+    }
+
+    @Test
+    void add() {
+        String string = "test string";
+        list.add(string);
+        assertEquals(string, list.get(5));
+    }
+
+    @Test
+    void addNull() {
+        assertThrows(InvalidItemException.class, () -> list.add(null));
+    }
+
+    @Test
+    void indexAdd() {
+        String string = "test string";
+        list.add(1, string);
+        assertEquals(string, list.get(1));
+        assertEquals("string", list.get(2));
+
+    }
+    @Test
+    void InvalidIndexAdd() {
+        assertThrows(InvalidIndexException.class, ()-> list.add(8,"string"));
+    }
+
+    @Test
+    void set() {
+        list.set(1,"more");
+        assertEquals("more",list.get(1));
+    }
+    @Test
+    void InvalidIndexSet() {
+        assertThrows(InvalidIndexException.class, ()-> list.set(8,"string"));
+    }
+
+    @Test
+    void removeIndex() {
+
+            list.remove(0);
+            assertEquals("string", list.get(0));
+    }
+    @Test
+    void InvalidIndexRemove() {
+        assertThrows(InvalidIndexException.class, ()-> list.remove(8));
+    }
+
+    @Test
+    void removeItem() {
+        list.remove("test");
+        assertEquals("string", list.get(0));
+    }
+    @Test
+    void removeNullItem() {
+        assertThrows(InvalidItemException.class, () -> list.remove(null));
+    }
+    @Test
+    void removeNotExistItem() {
+        assertThrows(ElementNotExistException.class, () -> list.remove("null"));
+    }
+
+    @Test
+    void contains() {
+        assertTrue(list.contains("string"));
+        assertFalse(list.contains("contains"));
+    }
+
+    @Test
+    void indexOf() {
+        assertEquals(1,list.indexOf("string"));
+    }
+
+    @Test
+    void indexOfNotExist() {
+        assertEquals(-1,list.indexOf("string1"));
+    }
+
+    @Test
+    void lastIndexOf() {
+        assertEquals(4,list.lastIndexOf("string"));
+    }
+    @Test
+    void lastIndexOfNotExist() {
+        assertEquals(-1,list.lastIndexOf("string1"));
+    }
+
+    @Test
+    void get() {
+        assertEquals("string",list.get(1));
+    }
+    @Test
+    void invalidIndexGet() {
+        assertThrows(InvalidIndexException.class, ()-> list.get(8));
+    }
+
+    @Test
+    void equals() {
+        StringList newList = new StringListImpl();
+        newList.add("test");
+        newList.add("string");
+        newList .add("list");
+        newList.add("word");
+        newList.add("string");
+        assertTrue(list.equals(newList));
+    }
+
+    @Test
+    void size() {
+        assertEquals(5,list.size());
+    }
+
+    @Test
+    void isEmpty() {
+        assertFalse(list.isEmpty());
+    }
+
+    @Test
+    void clear() {
+        list.clear();
+        assertEquals(0,list.size());
+    }
+
+    @Test
+    void toArray() {
+        String[] array = new String[] {"test","string","list","word","string"};
+        assertArrayEquals(array, list.toArray());
+    }
+}
